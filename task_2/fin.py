b44808e6c82965d29b8d26add817bd8d4140b57d
Greasy Finger <97301750+greasyfinger@users.noreply.github.com>
2024-02-03 19:40:02 +0530

    Add files via upload
    bigram models along with emotion scores

--- task_2/fin.py
+++ task_2/fin.py
@@ -1,40 +1,128 @@
 import numpy as np
-from transformers import BertTokenizer
+import pickle
+from utils import emotion_scores
+
+
+import warnings
+warnings.filterwarnings("ignore")
 
 class BigramLM:
     def get_tokens(self, seq):
-        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-        return tokenizer.convert_tokens_to_ids(tokenizer.tokenize(seq))
-
-    def get_seq(self, ids):
-        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-        return tokenizer.convert_ids_to_tokens(ids)
+        toks = seq.split()
+        return [self.tok_id[tok] for tok in toks]
     
-    def create_matrix(self, tokens):
+    def get_seq(self, ids):
+        tokid = [self.id_tok[id] for id in ids]
+        sent = "".join(tokid)
+        return sent
+    
+    def create_mat(self, tokens):
         max_tkn = max(tokens) + 1
-        coeff_mat = np.zeros((max_tkn, max_tkn), dtype=np.int32)
+        coeff_mat = np.zeros((max_tkn, max_tkn), dtype=np.float64)
         for t1, t2 in zip(tokens, tokens[1:]):
             coeff_mat[t1, t2] += 1
-        return coeff_mat
+        return coeff_mat/coeff_mat.sum(1, keepdims=True)
     
-    def __init__(self, data):
-        self.token_data = self.get_tokens(data)
-        self.coeff_mat = self.create_matrix(self.token_data)
+    def laplace_mat(self, tokens):
+        max_tkn = max(tokens) + 1
+        coeff_mat = np.zeros((max_tkn, max_tkn), dtype=np.float64)
+        for t1, t2 in zip(tokens, tokens[1:]):
+            coeff_mat[t1, t2] += 1
+        coeff_mat += 1
+        return coeff_mat/coeff_mat.sum(1, keepdims=True)
     
-    def genrate(self, seq_ln, strt = 1045):
-        idx = strt
+    def kneser_ney_mat(self, tokens, d = 0.75):
+        max_tkn = max(tokens) + 1
+        coeff_mat = np.zeros((max_tkn, max_tkn), dtype=np.float64)
+        for t1, t2 in zip(tokens, tokens[1:]):
+            coeff_mat[t1, t2] += 1
+        frst_trm = np.maximum(coeff_mat - d, 0)/coeff_mat.sum(1, keepdims=True)
+        lmbd_trm = (d/coeff_mat.sum(axis = 1,keepdims=True)) * np.count_nonzero(coeff_mat, axis=1, keepdims=True)
+        cont = np.count_nonzero(coeff_mat, axis=0, keepdims=True)
+        pcnt_trm = cont/ cont.sum()
+        return frst_trm + lmbd_trm * pcnt_trm
+    
+    def genrate(self, size = -1):
+        idx = self.get_tokens("<s>")[0]
+        generated_sequence = ""
         i = 0
-        while True:
-            print(self.get_seq(idx))
-            if(i == seq_ln):
+        while (self.get_seq([idx]) != "<e>"):
+            if(i == size):
+                generated_sequence += "..."
                 break
-            p  = self.coeff_mat[idx]
-            p = p/p.sum()
-            idx = np.random.choice(len(p), p=p)
+            elif(i != 0):
+                generated_sequence += self.get_seq([idx]) + " "
+            p = self.coeff_mat[idx]
+            idx = int(np.random.choice(len(p), p=p))
             i += 1
+        return generated_sequence
+    
+    def __init__(self, file, smoothing = 0):
+
+        self.tokens = set()
+        self.data = ""
+
+        with open(file, 'r') as fil:
+            data = fil.read()
+        lin_data = data.split("\n")
+        for sntc in lin_data:
+            fsntc = f'<s> {sntc} <e> '
+            self.data += fsntc
+            self.tokens.update(fsntc.split())
+        
+        self.tok_id = {token: index for index, token in enumerate(self.tokens)}
+        self.id_tok = {index: token for token, index in self.tok_id.items()}
+        self.tokenized = self.get_tokens(self.data)
+
+        if(smoothing == 1):
+            self.coeff_mat = self.laplace_mat(self.tokenized)
+        elif(smoothing == 2):
+            self.coeff_mat = self.kneser_ney_mat(self.tokenized)
+        else:
+            self.coeff_mat = self.create_mat(self.tokenized)
+
+    def save(self, filename = "bigram_mdl.pkl"):
+        try:
+            with open(filename, 'wb') as f:
+                pickle.dump(self, f)
+            print(f"Object saved to '{filename}' successfully.")
+        except Exception as e:
+            print(f"Error occurred while saving object: {e}")
+
+    def get_emotion_score(self, size = -1):
+        sntnc = self.genrate(size = size)
+        emt_scr = emotion_scores(sntnc)
+        emote = max(emt_scr, key=lambda x: x['score'])
+        return sntnc, emote["label"], emote["score"]
+
+
+
+
+
+def load(filename = "bigram_mdl.pkl"):
+        try:
+            with open(filename, 'rb') as f:
+                obj = pickle.load(f)
+            print(f"Object retrieved from '{filename}' successfully.")
+            return obj
+        except Exception as e:
+            print(f"Error occurred while retrieving object: {e}")
+
+
+
 
         
-with open('corpus.txt', 'r') as fil:
-    inp_data = fil.read()
-bgram = BigramLM(inp_data)
-bgram.genrate(10)
+
+bgram = BigramLM('corpus.txt',2)
+sent, label, score = bgram.get_emotion_score(size = 15)
+print(sent)
+print(label, " - ", score)
+
+
+
+
+bgram.save()
+
+
+# bgram = load()
+# bgram.genrate(10)
